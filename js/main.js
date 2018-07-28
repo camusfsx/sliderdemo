3f189544b6f1f1053255da085c2cf6dc4cc2a344
camus <f1992863292@outlook.com>
2018-07-28 14:58:49 +0800

    finish

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-window.onload = () => {
-    let $imgBoard = $(`.imgBoard`)
+let $imgBoard = $(`.imgBoard`)
     , $icons = $(`.iconList>img`)
-    , carouselIndex = 0
-    $icons.on(`click`, (event) => {
-        let $icon = $(event.target)
-            , index = $icon.index()
-            , pixel = index * 275
-        $icon.addClass(`clicked`).siblings(`.clicked`).removeClass(`clicked`)
-        $imgBoard.css({ transform: `translateY(-${pixel}px)` })
-    })
-    setCarouselTimer.call(this, carouselIndex)
-    stopForHover.call(this, $imgBoard,carouselIndex)
-    stopForHover.call(this, $icons,carouselIndex)
-    function setCarouselTimer(index) {
-            carouselTimer = setInterval(() => {
-            index++
-            index = index > 3 ? 0 : index
-            $icons.eq(index).trigger(`click`)
-        }, 1000)
-    }
-    function stopForHover($object,index) {
-        $object.on(`mouseenter`, () => {
-            window.clearInterval(carouselTimer)
-        }).on(`mouseleave`, () => {
-            setCarouselTimer.call(this, index)
-        })
-    }
+    ,clickIndex = 0
+var carouselTimer = setCarouselTimer.call()
+$icons.on(`click`, (event) => {
+    let $icon = $(event.target)
+        , index = $icon.index()
+        , pixel = index * 275
+    $icon.addClass(`clicked`).siblings(`.clicked`).removeClass(`clicked`)
+    $imgBoard.css({ transform: `translateY(-${pixel}px)` })
+}) 
+
+stopForHover.call(this, $imgBoard)
+stopForHover.call(this, $icons)
+function setCarouselTimer(index) {
+    return setInterval(() => {
+        clickIndex++
+        clickIndex = clickIndex > 3 ? 0 : clickIndex
+        $icons.eq(clickIndex).trigger(`click`)
+    }, 1000)
 }
+function stopForHover($object) {
+    $object.on(`mouseenter`, () => {
+        window.clearInterval(carouselTimer)
+    }).on(`mouseleave`, () => {
+        carouselTimer = setCarouselTimer.call()
+    })
+}
+
 
 
 
